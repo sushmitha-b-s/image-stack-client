3251e546fee9601f6f5116e08153e418bcf2cd13
sushmitha-b-s <bssushmitha26@gmail.com>
2021-06-18 01:11:08 +0200

    Implemented form UI and modal styles

--- src/components/AddImageForm.vue
+++ src/components/AddImageForm.vue
@@ -1,5 +1,40 @@
 <template>
-	<div>
-		This is Add Image form component
-	</div>
+	<form class="form">
+		<div class="form__group">
+			<label for="arrayIndex" class="form__label">Array Index: </label>
+			<select name="arrayIndex" id="arrayIndex" class="form__select">
+				<option value="" class="form__select-option">Select an index</option>
+				<option
+					v-for="(arrayIndex, index) in arrayIndexes"
+					:key="index"
+					:arrayIndex="arrayIndex"
+					value="arrayIndex"
+					class="form__select-option"
+				>
+					{{ arrayIndex }}
+				</option>
+			</select>
+		</div>
+
+		<div class="form__group">
+			<label for="imageFile" class="form__label">Image file:</label>
+			<input type="file" name="imageFile" id="imageFile" class="form__file" />
+		</div>
+
+		<div class="form__btn">
+			<input type="submit" value="Add" class="btn btn--rect" />
+		</div>
+	</form>
 </template>
+
+<script>
+export default {
+	name: 'AddImageForm',
+
+	data() {
+		return {
+			arrayIndexes: [0, 1, 2, 3],
+		}
+	},
+}
+</script>
--- src/components/NavButtons.vue
+++ src/components/NavButtons.vue
@@ -1,22 +1,28 @@
 <template>
 	<div class="nav__btns">
-		<button class="nav__btn">generate</button>
-		<button class="nav__btn" @click="openModal">Add image</button>
+		<button class="nav__btn btn btn--round btn--move">generate</button>
+		<button class="nav__btn btn btn--round btn--move" @click="openModal">
+			Add image
+		</button>
 
 		<Modal>
 			<template slot="header">Add new image</template>
-			<template slot="body"> this is the body</template>
+			<template slot="body">
+				<AddImageForm />
+			</template>
 		</Modal>
 	</div>
 </template>
 
 <script>
-import Modal from './Modal.vue'
+import Modal from './Modal'
+import AddImageForm from './AddImageForm'
 
 export default {
 	name: 'NavButtons',
 	components: {
 		Modal,
+		AddImageForm,
 	},
 
 	data() {
